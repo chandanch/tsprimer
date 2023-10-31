557dde9e7383d1a19b675f1ec695340aafebdc4d
chandanch <chandan.cec107@gmail.com>
2023-10-31 09:15:36 +0530

    add outdir and ignore generated js

--- index.js
+++
@@ -1,4 +0,0 @@
-var calculateScore = function (score1, score2) {
-    return score1 + score2;
-};
-calculateScore(23, 2);
--- index.ts
+++ index.ts
@@ -2,4 +2,4 @@ const calculateScore = (score1: number, score2: number): number => {
   return score1 + score2;
 };
 
-calculateScore(23, 2);
+calculateScore(33, 2);
