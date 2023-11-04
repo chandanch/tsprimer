77d9296c7d5c5033e43bf4db749b4b91d141df26
chandanch <chandan.cec107@gmail.com>
2023-11-04 13:32:25 +0530

    type inference primer

---
+++ src/types-primer/typeInference.ts
@@ -0,0 +1,10 @@
+/**
+ * Type Inference: Ability of TS to automatically infer the type based on the value specified
+ * to the variable
+ */
+
+// infers the type as number
+const score = 2332;
+
+// infers the type as string
+const playerName = 'dddd';
