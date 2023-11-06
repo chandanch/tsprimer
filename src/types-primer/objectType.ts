3b2e3b76bfb7df2a1c344b160b6b55f32d5bc447
chandanch <chandan.cec107@gmail.com>
2023-11-06 08:53:10 +0530

    object types primer

---
+++ src/types-primer/objectType.ts
@@ -0,0 +1,15 @@
+/***
+ * Object Types - Enables specifying types for objecgt
+ */
+
+let car: {
+    name: string;
+    color: string;
+    price: number;
+};
+
+car = {
+    name: 'dw',
+    color: 'd',
+    price: 23,
+};
