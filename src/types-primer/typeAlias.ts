bfaf5030d88c38b049f4d01f06ee648b3e4502fd
chandanch <chandan.cec107@gmail.com>
2023-11-10 10:45:43 +0530

    nested objects and custom types

--- src/types-primer/typeAlias.ts
+++ src/types-primer/typeAlias.ts
@@ -1,4 +1,4 @@
-/**\
+/**
  * Type Alias: Allows us to define custom type for an object
  * We can define custom types using the type object
  */
@@ -15,4 +15,27 @@ let player: Player = {
     name: 'Deded',
     score: 122,
     userId: 2232,
+};
+
+// nested objects and custom types
+type Engine = {
+    rpm: Number;
+    model: string;
+    base: string;
+};
+
+type Vehicle = {
+    name: string;
+    make: string;
+    engine: Engine;
+};
+
+const dasCar: Vehicle = {
+    name: 'dswd',
+    make: 'dede',
+    engine: {
+        rpm: 2,
+        model: 'e',
+        base: 'dded',
+    },
 };
