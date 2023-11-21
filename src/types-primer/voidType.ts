434c0219a3561ab6a0c1b80d4dabda3800ef6b70
chandanch <chandan.cec107@gmail.com>
2023-11-21 09:59:34 +0530

    void types primer

---
+++ src/types-primer/voidType.ts
@@ -0,0 +1,9 @@
+/***
+ * Void types are assigned to functions.
+ * Void types does not return a value but executes all the code defined within the function
+ */
+
+// here log data is of void type
+function logData() {
+    console.log('logging data');
+}
