028797ddb82a5475c49115dd7cb8223cd5da22c1
chandanch <chandan.cec107@gmail.com>
2023-11-24 08:36:03 +0530

    update doc

---
+++ src/functions/basicFunctions.ts
@@ -0,0 +1,6 @@
+/***
+ * Functions in TS are syntactially similar to JS functions.
+ * TS functions allows us specify types for functions and enables us to specify types for
+ * function parameters.
+ * These types for the parameters are checked at the compile time when the function is invoked
+ */
