0a16d58dfdad375c395a40dfb9a89971f95aafb8
chandanch <chandan.cec107@gmail.com>
2023-11-28 22:29:45 +0530

    usage of generic with examples and doc desc :bulb:

--- src/generics/generics.ts
+++ src/generics/generics.ts
@@ -3,9 +3,10 @@
  * It enables the function or a property to accept any primitive or custom type
  * They are like placeholders for types that can be specified when the code is used.
  * This makes generics a great way to write reusable and type-safe code
- * Generics are defined using type parameters, which are placeholder names for types.
- *  These type parameters are enclosed in angle brackets (<>)
  *
+ * Generics are defined using type parameters, which are placeholder names for types.
+ * These type parameters are enclosed in angle brackets (<>)
+ * Generic code can be reused with different types of data without having to repeat yourself.
  */
 
 // Generic Function Example
@@ -15,7 +16,12 @@ function echoData<T>(arg: T): T {
     return arg;
 }
 
-// We have created a type alias or a cust
+// usage of echoData which takes a generic type param:
+// When the identity function is called, the type of the arg parameter will be
+// inferred from the type of the argument that is passed in
+const identityResult = echoData(10);
+
+// type alias or a custom type which takes in a generic type parameter
 type Filter = {
     <T>(item: T[], predicate: (item: T) => boolean): T[];
 };
@@ -30,4 +36,5 @@ const filter: Filter = (items, predicate) => {
     return results;
 };
 
+// here the type of filter() is inferred based on the type of values passed to the filter()
 console.log(filter([1, 2, 3, 6, 6], (item) => item > 3));
