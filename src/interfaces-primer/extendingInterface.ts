3668ab15d0463382ece116da47036fdd32eb7a0c
chandanch <chandan.cec107@gmail.com>
2023-12-30 17:13:14 +0530

    inheriting from multiple interface :bulb:

--- src/interfaces-primer/extendingInterface.ts
+++ src/interfaces-primer/extendingInterface.ts
@@ -24,3 +24,29 @@ const freeWing: FlyWingMotar = {
         return this.name;
     },
 };
+
+interface GreenConfer {
+    level: number;
+}
+
+enum GreeCategory {
+    catA = 'CategoryA',
+    catB = 'CategoryB',
+}
+
+// inheriting from multiple interfaces
+interface GreenWing extends FlyWingMotar, GreenConfer {
+    category: GreeCategory;
+}
+
+// specify the object type as GreenWing
+const wingsOfGreen: GreenWing = {
+    category: GreeCategory.catA,
+    generation: 'SS',
+    name: 'WingGreen',
+    level: 3,
+    type: 'TypeA',
+    generatorWing() {
+        return this.category;
+    },
+};
