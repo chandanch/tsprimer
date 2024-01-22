8e9bda1e7bf6135ee5f3a1bec1fd817e50d00305
chandanch <chandan.cec107@gmail.com>
2024-01-22 07:54:14 +0530

    use of callbacks and this context :bulb:

--- src/this-primer/thisPrimer.js
+++ src/this-primer/thisPrimer.js
@@ -16,7 +16,39 @@ const booker = {
         // this refers to booker object here
         console.log(this);
     },
+    authors: ['Sam', 'Dean', 'Pean'],
+
+    displayAuthors() {
+        this.authors.forEach(function (author) {
+            // this points to the window/global object here since the foreach function
+            // is bound to the window scope
+            /**
+             * The function passed to forEach is a regular anonymous function.
+             * In JavaScript, regular functions invoked in such a way (not as methods of an object)
+             * have their this context set to the global object (like window in a browser environment)
+             * or undefined if the code is running in strict mode
+             */
+            console.log(`${this.title} - ${author}`);
+        });
+    },
+
+    printAuthors() {
+        this.authors.forEach((author) => {
+            /**
+             * Arrow functions do not have their own this context;
+             * they inherit this from the enclosing lexical context.
+             * In this case, the arrow function inherits this from the printAuthors method,
+             * which is bound to the booker object.
+             * Therefore, this.title inside the arrow function correctly refers to the
+             * title of the booker object
+             */
+            console.log(`${this.title} - ${author}`);
+        });
+    },
 };
+
+booker.displayAuthors();
+booker.printAuthors();
 
 function filgger() {
     // this refer to global or window object here
