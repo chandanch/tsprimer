7bd9ebccd57ac635764321acfab87915b087f540
chandanch <chandan.cec107@gmail.com>
2024-02-04 10:23:46 +0530

    subtype and supertype example

--- src/advancedTS/subtypeSupertype.ts
+++ src/advancedTS/subtypeSupertype.ts
@@ -9,3 +9,27 @@
  * A subtype is a more specific type that inherits from a supertype.
  * It can have additional properties or methods while maintaining the properties and methods of its supertype.
  */
+
+/**
+ * Here, Animal is the supertype, and Bird is a subtype that extends Animal.
+ * Bird inherits properties like name and age from Animal and adds its own specific behavior
+ * (fly method).
+ */
+interface Animal {
+    name: string;
+    age: number;
+}
+
+interface Bird extends Animal {
+    fly(): void;
+}
+
+const myBird: Bird = {
+    name: 'Sparrow',
+    age: 2,
+    fly: () => {
+        console.log('Flying...');
+    },
+};
+
+let animal: Animal = myBird; // OK, Bird is a subtype of Animal
