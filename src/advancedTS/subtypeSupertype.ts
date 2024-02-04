ea2a9fd6905e24cfb906b8a30763687731d95dec
chandanch <chandan.cec107@gmail.com>
2024-02-04 10:15:14 +0530

    supertype & subtype overview

---
+++ src/advancedTS/subtypeSupertype.ts
@@ -0,0 +1,11 @@
+/**
+ * subtypes and supertypes are related to the concept of inheritance and type relationships.
+ * TypeScript is a statically typed superset of JavaScript that supports the creation of
+ * complex types through interfaces, classes, and other constructs
+ *
+ * A supertype is a more general or broader type that can represent a larger set of values.
+ * It is often used as a base or parent type for more specific types.
+ *
+ * A subtype is a more specific type that inherits from a supertype.
+ * It can have additional properties or methods while maintaining the properties and methods of its supertype.
+ */
