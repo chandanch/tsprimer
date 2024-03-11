2520b0a8281e116bc02062192b4bbef7aec02448
chandanch <chandan.cec107@gmail.com>
2024-03-11 09:12:34 +0530

    update docs and usage of satisfies :bulb:

--- src/advancedTS/satisfiesOperator.ts
+++ src/advancedTS/satisfiesOperator.ts
@@ -1,12 +1,13 @@
-type ColorProperties = 'red' | 'green' | 'blue';
-type RGBColor = [red: number, green: number, blue: number];
+/**
+ * The satisfies operator is used in type assertions to perform a check
+ * where you want to ensure that a value or object structure conforms to a specific type
+ */
 
 /**
  * Record type is a utility type that allows you to create an object type
  * with specified keys and a uniform value type. It's particularly useful when you need to
  * define an object type with known keys and a consistent value type across all keys
  */
-
 type FruitPrices = Record<string, number>;
 
 const prices: FruitPrices = {
@@ -14,6 +15,9 @@ const prices: FruitPrices = {
     banana: 0.99,
     orange: 2.49,
 };
+
+type ColorProperties = 'red' | 'green' | 'blue';
+type RGBColor = [red: number, green: number, blue: number];
 
 const colorPalete = {
     red: [255, 0, 1],
