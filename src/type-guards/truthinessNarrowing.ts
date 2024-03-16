ce68b6a0d8e765ad7af7bd0024cd162c4f69f341
chandanch <chandan.cec107@gmail.com>
2024-03-16 23:10:58 +0530

    update desc

--- src/type-guards/truthinessNarrowing.ts
+++ src/type-guards/truthinessNarrowing.ts
@@ -3,6 +3,7 @@ type HilMan = {
     age?: number;
 };
 
+// With truthiness narrowing TS checks the type of the property or a variable automatically
 const displayDetails = (hilman: HilMan) => {
     if (hilman.age) {
         console.log(hilman.age);
