9d0baa2722b57b506599555fd43f0231c9af6634
chandanch <chandan.cec107@gmail.com>
2024-03-16 08:33:59 +0530

    add doc

--- src/type-guards/typeofTypeGuards.ts
+++ src/type-guards/typeofTypeGuards.ts
@@ -1,4 +1,6 @@
 function printLogDetails(log: string | string[] | null) {
+    // type check or type guard to check possibility of null value
+    // TS will throw error if null check isn't performed
     if (!log) {
         console.log('Log data must be passed');
     } else if (typeof log == 'object') {
