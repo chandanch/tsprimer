754aed0fb46fd5665c7643b74b37e47d28472883
chandanch <chandan.cec107@gmail.com>
2024-03-18 14:19:19 +0200

    update doc

--- src/type-guards/typeguardPrimer.js
+++ src/type-guards/typeguardPrimer.js
@@ -1,7 +1,7 @@
 // Here we use typeof operator to check the type of the argument before we convert to string
 // In JS, this is a type guard or type safe mechanism where we first check for the type and then perfom the
 // needed action
-// This enables type checking and makes it type safe
+// This enables type checking and makes it type safe and guard against the type checks
 function convertToString(score) {
     if (typeof score !== 'number') {
         throw new Error('Invalid Type');
