917cf3a7135f5f33a0674ab5ad1638243e0b92da
chandanch <chandan.cec107@gmail.com>
2024-03-19 10:05:01 +0530

    use of in operator narrowing :bulb:

---
+++ src/type-guards/inOperatorNarrowing.ts
@@ -0,0 +1,31 @@
+/**
+ * With `in` operator narrowing we can check if the property exists in a type and then perform
+ * operations based on it.
+ */
+
+type MillCircle = {
+    kind: 'circle';
+    radius: number;
+};
+
+type MillSquare = {
+    kind: 'square';
+    length: number;
+};
+
+type MillShape = MillCircle | MillSquare;
+
+const milCalculateArea = (shape: MillShape) => {
+    if ('radius' in shape) {
+        return shape.radius * 3.14;
+    } else {
+        return shape.length ** 2;
+    }
+};
+
+const JiMillSquare: MillCircle = {
+    kind: 'circle',
+    radius: 34,
+};
+
+milCalculateArea(JiMillSquare);
