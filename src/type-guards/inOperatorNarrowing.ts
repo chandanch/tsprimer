913d90d45cfdfa8c5e60a4eec3abe79f00ad10df
chandanch <chandan.cec107@gmail.com>
2024-03-20 21:54:16 +0530

    use of union type

---
+++ src/type-guards/inOperatorNarrowing.ts
@@ -0,0 +1,39 @@
+/**
+ * With `in` operator narrowing we can check if the property exists in a type and then perform
+ * operations based on it.
+ */
+
+type MillCircle = {
+    kind: 'circle';
+    radius: number;
+};
+
+type MillSquare = {
+    kind: 'square';
+    length: number;
+};
+
+type MillShape = MillCircle | MillSquare;
+
+const milCalculateArea = (shape: MillShape) => {
+    // use of in operator to check existence of a property
+    if ('radius' in shape) {
+        return shape.radius * 3.14;
+    } else {
+        return shape.length ** 2;
+    }
+};
+
+const JiMillCircle: MillCircle = {
+    kind: 'circle',
+    radius: 34,
+};
+
+milCalculateArea(JiMillCircle);
+
+const JiMillSqaure: MillShape = {
+    kind: 'square',
+    length: 3,
+};
+
+milCalculateArea(JiMillSqaure);
