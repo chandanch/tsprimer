dd36df9faa1467f3a1b28c77518829baf9d4eefb
chandanch <chandan.cec107@gmail.com>
2024-04-11 13:31:25 +0530

    use of partial utility type

---
+++ src/builtInUtilTypes/partialType.ts
@@ -0,0 +1,17 @@
+/**
+ * Partial Utility type makes all the properties of an interface or a type as optional
+ * Syntax:
+ * Partial<Type>
+ * The partial keyword accepts a generic type parameter and returns a new type with all
+ * properties marked as optional (?)
+ */
+
+interface JislManDetails {
+    name: string;
+    age: number;
+    role: string;
+    email: string;
+}
+
+// creating a new type with all properties as optional using Partial utility type
+type PartailJisMan = Partial<JislManDetails>;
